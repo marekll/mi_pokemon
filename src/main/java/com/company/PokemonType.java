dbffa406859b7525d95c9ebf37d1b5062be57cfe
Marek <maro99999999@wp.pl>
2021-03-26 22:39:31 +0100

    Deleted unnecessary method from pokemon type

--- src/main/java/com/company/PokemonType.java
+++ src/main/java/com/company/PokemonType.java
@@ -5,10 +5,6 @@ import javafx.util.Pair;
 public class PokemonType {
     String name;
     String url;
-    @Override
-    public boolean equals(Object o){
-        return this.name.equals(((PokemonType) o).getName());
-    }
     public String getName(){
         return name;
     }
